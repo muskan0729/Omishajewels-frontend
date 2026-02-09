d36860e35d89015d4d3a71c2aa1671f6fbba551e
Intern-003 <intern@spay.live>
2026-02-09 11:28:28 +0530

    Product details and Merge Issues resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Toaster } from "sonner";
-
-
-/* COMMON */
 import { useState } from "react";
 
 /* COMMON */
@@ -25,6 +22,8 @@ import TermsConditions from "./pages/TermsConditions";
 import RefundCancellation from "./pages/RefundCancellation";
 import ReturnPolicy from "./pages/ReturnPolicy";
 import ShopPage from "./components/shop/ShopPage";
+import WishlistPage from "./pages/my-account/Wishlist";
+import ProductDetails from "./pages/ProductDetails";
 
 /* MY ACCOUNT */
 import MyAccountLayout from "./pages/my-account/MyAccountLayout";
@@ -35,133 +34,107 @@ import Addresses from "./pages/my-account/Addresses";
 import AccountDetails from "./pages/my-account/AccountDetails";
 import AccountWishlist from "./pages/my-account/Wishlist";
 import Logout from "./pages/my-account/Logout";
-import Addresses from "./pages/my-account/Addresses";
 import EditBillingAddress from "./pages/my-account/EditBillingAddress";
 import EditShippingAddress from "./pages/my-account/EditShippingAddress";
 
+/* CART */
 import Cartprocess from "./components/Cartprocess";
-import ShopPage from "./components/shop/ShopPage";
 
-
+/* ADMIN */
 import AdminLayout from "./components/common/AdminLayout";
 import Dashboard from "./pages/admin/Dashboard";
 import Categories from "./pages/admin/Categories";
 import Ebooks from "./pages/admin/Ebooks";
 import Allorders from "./pages/admin/Allorders";
-// import Users from "./pages/admin/Users";
+import Users from "./pages/admin/Users";
+import Transactions from "./pages/admin/Transactions";
 // import Images from "./pages/admin/Images";
-import Images from "./pages/admin/Images";
-import AuthSidebar from "./components/auth/AuthSidebar";
-
-
-
 
 function App() {
   const [authOpen, setAuthOpen] = useState(false);
   const [authView, setAuthView] = useState("login");
 
-  const openLogin = () => {
-    setAuthView("login");
-    setAuthOpen(true);
-  };
-
   return (
     <>
-     <Toaster position="bottom-left" richColors closeButton />
-   
-    <BrowserRouter>
-      <Routes>
-        {/* PUBLIC ROUTES */}
-        <Route 
-          path="/*" 
-          element={
-            <>
+      <Toaster position="bottom-left" richColors closeButton />
 
-       <Header
-        openLogin={() => {
-          setAuthView("login");
-          setAuthOpen(true);
-        }}
-      />
+      <BrowserRouter>
+        <Routes>
+          {/* ================= PUBLIC + ACCOUNT LAYOUT ================= */}
+          <Route
+            path="/*"
+            element={
+              <>
+                <Header
+                  openLogin={() => {
+                    setAuthView("login");
+                    setAuthOpen(true);
+                  }}
+                />
 
-      {/* AUTH SIDEBAR (GLOBAL, NOT A ROUTE) */}
-      <AuthSidebar
-        open={authOpen}
-        setOpen={setAuthOpen}
-        view={authView}
-        setView={setAuthView}
-      />
+                {/* AUTH SIDEBAR */}
+                <AuthSidebar
+                  open={authOpen}
+                  setOpen={setAuthOpen}
+                  view={authView}
+                  setView={setAuthView}
+                />
 
-      {/* AUTH SIDEBAR (GLOBAL, NOT A ROUTE) */}
-      <AuthSidebar
-        open={authOpen}
-        setOpen={setAuthOpen}
-        view={authView}
-        setView={setAuthView}
-      />
+                <main className="min-h-screen bg-[#FEFCF9]">
+                  <Routes>
+                    {/* PUBLIC ROUTES */}
+                    <Route path="/" element={<Home />} />
+                    <Route path="/shop" element={<ShopPage />} />
+                    <Route path="/view-cart" element={<ViewCart />} />
+                    <Route path="/checkout" element={<Checkout />} />
+                    <Route path="/order" element={<OrderComplete />} />
 
-              <main className="min-h-screen bg-[#FEFCF9]">
-                <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/view-cart" element={<ViewCart />} />
-                  {/* ... all your public routes */}
-                  <Route path="/checkout" element = {<Checkout />} />
-           <Route path="/order" element = {<OrderComplete />} />
-         <Route path="/shop" element = {<ShopPage />} />
+                    <Route path="/about" element={<AboutUs />} />
+                    <Route path="/contact" element={<ContactUs />} />
+                    <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+                    <Route path="/shipping-policy" element={<ShippingPolicy />} />
+                    <Route path="/terms" element={<TermsConditions />} />
+                    <Route path="/refund-policy" element={<RefundCancellation />} />
+                    <Route path="/return-policy" element={<ReturnPolicy />} />
+                    <Route path="/wishlist" element={<WishlistPage />} />
 
-          <Route path="/about" element={<AboutUs />} />
-          <Route path="/contact" element={<ContactUs />} />
-          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-          <Route path="/shipping-policy" element={<ShippingPolicy />} />
-          <Route path="/terms" element={<TermsConditions />} />
-          <Route path="/refund-policy" element={<RefundCancellation />} />
-          <Route path="/return-policy" element={<ReturnPolicy />} />
-          <Route path="/wishlist" element={<WishlistPage />} />
+                    {/* MY ACCOUNT */}
+                    <Route path="/my-account" element={<MyAccountLayout />}>
+                      <Route index element={<DashboardMyAccount />} />
+                      <Route path="orders" element={<Orders />} />
+                      <Route path="downloads" element={<Downloads />} />
+                      <Route path="addresses" element={<Addresses />} />
+                      <Route path="account-details" element={<AccountDetails />} />
+                      <Route path="wishlist" element={<AccountWishlist />} />
+                      <Route path="logout" element={<Logout />} />
+                      <Route path="edit-address/billing" element={<EditBillingAddress />} />
+                      <Route path="edit-address/shipping" element={<EditShippingAddress />} />
+                    </Route>
 
-          {/* MY ACCOUNT ROUTES */}
-        <Route path="/my-account" element={<MyAccountLayout />}>
-  <Route index element={<DashboardMyaccont />} />
-  <Route path="orders" element={<Orders />} />
-  <Route path="downloads" element={<Downloads />} />
-  <Route path="addresses" element={<Addresses />} />
-  <Route path="account-details" element={<AccountDetails />} />
-  <Route path="wishlist" element={<AccountWishlist />} />
-  <Route path="logout" element={<Logout />} />
-  <Route path="edit-address" element={<Addresses />} />
-<Route path="edit-address/billing" element={<EditBillingAddress />} />
-<Route path="edit-address/shipping" element={<EditShippingAddress />} />
-</Route>
+<Route path="/product/:slug" element={<ProductDetails />} />
+                    <Route path="/cartprocess" element={<Cartprocess />} />
+                  </Routes>
+                </main>
 
-<Route path="/my-account" element={<MyAccountLayout />}>
-  <Route index element={<DashboardMyaccont />} />
+                <Footer />
+              </>
+            }
+          />
 
-  <Route path="edit-address" element={<Addresses />} />
-  <Route path="edit-address/billing" element={<EditBillingAddress />} />
-  <Route path="edit-address/shipping" element={<EditShippingAddress />} />
-</Route>
- <Route path="/Cartprocess" element={<Cartprocess />} />
-      
-                </Routes>
-              </main>
-              <Footer />
-            </>
-          } 
-        />
-
-        {/* ADMIN ROUTES - SEPARATE LAYOUT */}
-        {/* <Route path="/admin/*" element={<AdminLayout />}>
-          <Route index element={<Dashboard />} />
-          <Route path="categories" element={<Categories />} />
-          <Route path="ebooks" element={<Ebooks />} />
-          <Route path="orders" element={<Allorders />} />
-          <Route path="users" element={<Users />} />
-          <Route path="transactions" element={<Transactions />} />
-        </Route>
-      </Routes> */}
-    </BrowserRouter>
-      </>
+          {/* ================= ADMIN ROUTES ================= */}
+          <Route path="/admin/*" element={<AdminLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="categories" element={<Categories />} />
+            <Route path="ebooks" element={<Ebooks />} />
+            <Route path="orders" element={<Allorders />} />
+            <Route path="users" element={<Users />} />
+            <Route path="transactions" element={<Transactions />} />
+            {/* <Route path="images" element={<Images />} /> */}
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
 export default App;
-
